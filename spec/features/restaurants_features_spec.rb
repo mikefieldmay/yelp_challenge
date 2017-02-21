21baf00cb91078e66df325982a47702f516fbd78
Kat Hicks <kmhicks92@gmail.com>
2017-02-21 15:24:04 +0000

    added feature for editing an existing restaurant

--- spec/features/restaurants_features_spec.rb
+++ spec/features/restaurants_features_spec.rb
@@ -32,14 +32,28 @@ feature 'restaurants' do
   end
 
   context 'viewing restaurants' do
-
   let!(:kfc){ Restaurant.create(name:'KFC') }
-
-  scenario 'lets a user view a restaurant' do
-     visit '/restaurants'
-     click_link 'KFC'
-     expect(page).to have_content 'KFC'
-     expect(current_path).to eq "/restaurants/#{kfc.id}"
+    scenario 'lets a user view a restaurant' do
+       visit '/restaurants'
+       click_link 'KFC'
+       expect(page).to have_content 'KFC'
+       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
   end
+
+  context 'editing restaurants' do
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness', id: 1 }
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'KFC'
+      fill_in 'Description', with: 'Deep fried goodness!!'
+      click_button 'Update Restaurant'
+      click_link 'KFC'
+      expect(page).to have_content 'KFC'
+      expect(page).to have_content 'Deep fried goodness!!'
+      expect(current_path).to eq '/restaurants/1'
+    end
+  end
+
 end
