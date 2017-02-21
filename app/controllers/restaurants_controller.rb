b0b890cd48d40bb85ec5894ead7f1a4f948110ad
Kat Hicks <kmhicks92@gmail.com>
2017-02-21 12:22:31 +0000

    added form to add new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,22 @@
 class RestaurantsController < ApplicationController
-  def index
+
+  def index #GET
     @restaurants = Restaurant.all
   end
+
+  def new #GET
+    @restaurant = Restaurant.new
+  end
+
+  def create #POST
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
